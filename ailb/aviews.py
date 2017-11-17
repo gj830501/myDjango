33021b513c09cc3bba5c835ac901fce9eb2ed923
gj850501 <gj830501@163.com>
2017-11-17 16:36:51 +0800

    查询服务类

--- ailb/aviews.py
+++ ailb/aviews.py
@@ -3,6 +3,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 # Create your views here.
 from django.shortcuts import HttpResponse
+
 #测试用随后删除
 def test(request):
    #用HttpResponse返回字符串
@@ -10,5 +11,6 @@ def test(request):
 
 #首页
 def index(request):
+
    #用HttpResponse返回字符串
     return render(request,'alib/index.html')
--- ailb/service/indexService.py
+++ ailb/service/indexService.py
@@ -4,9 +4,9 @@
 # @Author: Eddie
 # @Date  : 2017/11/17 16:18
 # @Desc  : 首页业务处理类
-from ailb.models import TechEssay
+from ailb import models
 class indexService(object):
 
     def getEssayInfo(self,start,end):
         if start == 0 and end == 0:
-            TechEssay = models.TechEssay.objects.all().filter(id > 6 )
+            techEssay = models.TechEssay.objects.all().filter(id > 6 )
